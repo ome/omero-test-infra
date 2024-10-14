744995b25ac9c0fa23a997fabb0bcc903b4f43f2
Mariana Meireles <meireles@evolbio.mpg.de>
2024-10-14 18:16:24 +0200

    Ive messed up

--- ontop/run_ontop.sh
+++ ontop/run_ontop.sh
@@ -6,4 +6,9 @@ OBDA_FILE=/usr/local/bin/ontop-config/omemap.obda
 PROPERTIES_FILE=/usr/local/bin/ontop-config/omemap.properties
 
 echo "Launching Ontop..."
-exec /usr/local/bin/ontop-config endpoint --ontology=$OWL_FILE --mapping=$OBDA_FILE --properties=$PROPERTIES_FILE
+echo "====================================================="
+ls /usr/local/bin/
+echo "====================================================="
+ls /usr/local/
+echo "====================================================="
+exec /usr/local/bin/ontop endpoint --ontology=$OWL_FILE --mapping=$OBDA_FILE --properties=$PROPERTIES_FILE
