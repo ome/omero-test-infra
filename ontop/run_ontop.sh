9e6f0c1ac41901d782a1c13a818f6001eaab3b01
Mariana Meireles <meireles@evolbio.mpg.de>
2024-10-14 18:22:29 +0200

    I hate the cloud

--- ontop/run_ontop.sh
+++ ontop/run_ontop.sh
@@ -6,9 +6,4 @@ OBDA_FILE=/usr/local/bin/ontop-config/omemap.obda
 PROPERTIES_FILE=/usr/local/bin/ontop-config/omemap.properties
 
 echo "Launching Ontop..."
-echo "====================================================="
-ls /usr/local/bin/
-echo "====================================================="
-ls /usr/local/
-echo "====================================================="
-exec /usr/local/bin/ontop endpoint --ontology=$OWL_FILE --mapping=$OBDA_FILE --properties=$PROPERTIES_FILE
+exec ontop-cli/ontop endpoint --ontology=$OWL_FILE --mapping=$OBDA_FILE --properties=$PROPERTIES_FILE
