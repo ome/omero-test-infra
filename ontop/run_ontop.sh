d628dced75dc49cade648dcc79c535ba811c7b4e
Mariana Meireles <meireles@evolbio.mpg.de>
2024-10-14 18:09:14 +0200

    This all shouldn't be going to bin but i dont think it's a big deal so

--- ontop/run_ontop.sh
+++ ontop/run_ontop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-OWL_FILE=/usr/local/ontop-config/omemap.ttl
-OBDA_FILE=/usr/local/ontop-config/omemap.obda
-PROPERTIES_FILE=/usr/local/ontop-config/omemap.properties
+OWL_FILE=/usr/local/bin/ontop-config/omemap.ttl
+OBDA_FILE=/usr/local/bin/ontop-config/omemap.obda
+PROPERTIES_FILE=/usr/local/bin/ontop-config/omemap.properties
 
 echo "Launching Ontop..."
-exec /usr/local/ontop-config endpoint --ontology=$OWL_FILE --mapping=$OBDA_FILE --properties=$PROPERTIES_FILE
+exec /usr/local/bin/ontop-config endpoint --ontology=$OWL_FILE --mapping=$OBDA_FILE --properties=$PROPERTIES_FILE
