fd044fdf84223c2774d9dcf1fcd8954399dd3813
Mariana Meireles <meireles@evolbio.mpg.de>
2024-10-15 16:12:53 +0200

    Add some debugging

--- ontop/run_ontop.sh
+++ ontop/run_ontop.sh
@@ -5,5 +5,8 @@ OWL_FILE=/usr/local/bin/ontop-config/omemap.ttl
 OBDA_FILE=/usr/local/bin/ontop-config/omemap.obda
 PROPERTIES_FILE=/usr/local/bin/ontop-config/omemap.properties
 
+echo "🐷🐷🐷🐷🐷🐷🐷"
+mvn help:effective-pom | grep maven-compiler-plugin
+
 echo "Launching Ontop..."
 exec ontop-cli/ontop endpoint --ontology=$OWL_FILE --mapping=$OBDA_FILE --properties=$PROPERTIES_FILE
