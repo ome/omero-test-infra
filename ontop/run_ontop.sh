96785e99e1924993a3b811b0cab2d24914565c14
Mariana Meireles <meireles@evolbio.mpg.de>
2024-10-15 15:30:01 +0200

    Add some debugging

--- ontop/run_ontop.sh
+++ ontop/run_ontop.sh
@@ -5,5 +5,26 @@ OWL_FILE=/usr/local/bin/ontop-config/omemap.ttl
 OBDA_FILE=/usr/local/bin/ontop-config/omemap.obda
 PROPERTIES_FILE=/usr/local/bin/ontop-config/omemap.properties
 
+echo "==============================================================="
+echo "Checking whether Java and Postgres are running and operational"
+echo "Whats in the port where sql should be?"
+netstat -tuln | grep -q ":5432"
+# Check if Java is running
+if pgrep -x "java" > /dev/null
+then
+    echo "Java is running"
+else
+    echo "Java is not running"
+fi
+
+# Check if Postgres is running
+if pgrep -x "postgres" > /dev/null
+then
+    echo "Postgres is running"
+else
+    echo "Postgres is not running"
+fi
+echo "==============================================================="
+
 echo "Launching Ontop..."
 exec ontop-cli/ontop endpoint --ontology=$OWL_FILE --mapping=$OBDA_FILE --properties=$PROPERTIES_FILE
