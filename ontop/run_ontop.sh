4d3fe6f3e1f22633f8ebb38f47ee2ecb14ba214b
Mariana Meireles <meireles@evolbio.mpg.de>
2024-10-15 15:34:13 +0200

    Add some debugging

--- ontop/run_ontop.sh
+++ ontop/run_ontop.sh
@@ -8,6 +8,7 @@ PROPERTIES_FILE=/usr/local/bin/ontop-config/omemap.properties
 echo "==============================================================="
 echo "Checking whether Java and Postgres are running and operational"
 echo "Whats in the port where sql should be?"
+apt update && apt install -y net-tools
 netstat -tuln | grep -q ":5432"
 # Check if Java is running
 if pgrep -x "java" > /dev/null
