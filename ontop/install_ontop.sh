30cb181d449c9a8cb48b8077bbf3e5f8309865d3
Mariana Meireles <meireles@evolbio.mpg.de>
2024-10-17 11:54:09 +0200

    Rm debug, let scripts fail...

--- ontop/install_ontop.sh
+++ ontop/install_ontop.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
-set -e
+# set -e
 
 INSTALL_DIR="ontop-cli"
 ZIP_FILE="ontop-cli-5.2.0.zip"
 JDBC_DRIVER="postgresql-42.7.4.jar"
 
-# Check whether Ontop CLI is already installed
-if [ ! -d "$INSTALL_DIR" ]; then
-    echo "Ontop CLI is not installed. Installing Ontop CLI..."
-    wget https://github.com/ontop/ontop/releases/download/ontop-5.2.0/$ZIP_FILE
-    mkdir -p $INSTALL_DIR
-    unzip $ZIP_FILE -d $INSTALL_DIR
-    rm $ZIP_FILE
+echo "Installing Ontop CLI..."
+wget https://github.com/ontop/ontop/releases/download/ontop-5.2.0/$ZIP_FILE
+mkdir -p $INSTALL_DIR
+unzip $ZIP_FILE -d $INSTALL_DIR
+rm $ZIP_FILE
 
-    # Download JDBC driver for PostgreSQL
-    wget https://jdbc.postgresql.org/download/$JDBC_DRIVER -P $INSTALL_DIR/jdbc
-else
-    echo "Ontop CLI is already installed."
-fi
+echo "Download JDBC driver for PostgreSQL"
+wget https://jdbc.postgresql.org/download/$JDBC_DRIVER -P $INSTALL_DIR/jdbc
 
 echo "Ontop installation completed."
