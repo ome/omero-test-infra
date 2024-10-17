c06bfc011fc938458f2c9cb5f534ec390d75bae0
Mariana Meireles <meireles@evolbio.mpg.de>
2024-10-17 12:38:39 +0200

    Add debug + change the address of the db connection on ontop

--- ontop/run_ontop.sh
+++ ontop/run_ontop.sh
@@ -5,6 +5,11 @@ OWL_FILE=/usr/local/bin/ontop-config/omemap.ttl
 OBDA_FILE=/usr/local/bin/ontop-config/omemap.obda
 PROPERTIES_FILE=/usr/local/bin/ontop-config/omemap.properties
 
+echo "🌸🌸🌸🌸🌸🌸🌸"
+echo "testing connection with db"
+ping db
+echo "🌸🌸🌸🌸🌸🌸🌸"
+
 echo "Launching Ontop..."
 exec ontop-cli/ontop endpoint --ontology=$OWL_FILE --mapping=$OBDA_FILE --properties=$PROPERTIES_FILE
 
