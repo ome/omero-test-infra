4d8bd7d26900e962a797f37167496c9c6fc1ed79
Mariana Meireles <meireles@evolbio.mpg.de>
2024-10-27 20:41:32 +0100

    Specify mamba_root_prefix in the micromamba init thing

--- ontop/install_omero_py.sh
+++ ontop/install_omero_py.sh
@@ -6,21 +6,28 @@ set -e
 apt-get update
 apt-get install -y curl tar xz-utils bzip2
 
+# Create a directory for Micromamba binaries
+mkdir -p ~/micromamba/bin
 
-# Install Micromamba using the installation script via wget
+# Install Micromamba using the installation script
 echo 'curl 🐌🐌🐌'
-curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
-ls
+curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj -C ~/micromamba/bin bin/micromamba
 
-eval "$(./bin/micromamba shell hook -s posix)"
+# Add Micromamba to PATH
+export PATH="$HOME/micromamba/bin:$PATH"
 
-./bin/micromamba shell init -s bash -r ~/micromamba
-source ~/.bashrc
+# Set the root prefix to a different directory
+export MAMBA_ROOT_PREFIX="$HOME/mamba-root"
 
-micromamba activate
+# Initialize Micromamba shell with the new root prefix
+eval "$(micromamba shell hook -s posix)"
+micromamba shell init -s bash -p "$MAMBA_ROOT_PREFIX"
 
-# Create the base environment with Micromamba
+# Create and activate the base environment
 micromamba create -y -n base python=3.9.15
+
+# Activate the base environment (in non-interactive mode)
+micromamba activate base
 
 # Configure to use conda-forge channel by default
 micromamba config append channels conda-forge
