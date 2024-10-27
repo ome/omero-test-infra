2dd1eab3ddf656bdbf2e82a4234d8b3b29518407
Mariana Meireles <meireles@evolbio.mpg.de>
2024-10-27 19:22:31 +0100

    Trying micromamba with the stuff from the website

--- ontop/install_omero_py.sh
+++ ontop/install_omero_py.sh
@@ -3,23 +3,30 @@
 # Stop the script if any step fails
 set -e
 
+apt-get update
+apt-get install -y curl tar xz-utils
+
+# Create a directory for Micromamba
+mkdir -p ~/micromamba
+
 # Install Micromamba using the installation script via wget
-wget -qO- https://micro.mamba.pm/install.sh | bash
+curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
 
 # Add Micromamba to PATH
 export PATH="$HOME/micromamba/bin:$PATH"
 
 # Initialize Micromamba shell
-eval "$(micromamba shell hook -s bash)"
+eval "$(./bin/micromamba bash hook -s posix)"
+./bin/micromamba bash init -s bash -r ~/micromamba
+source ~/.bashrc
+
+micromamba activate
 
 # Create the base environment with Micromamba
 micromamba create -y -n base python=3.9.15
 
-# Activate the base environment
-micromamba activate base
-
 # Configure to use conda-forge channel by default
-micromamba config set channels conda-forge
+micromamba config append channels conda-forge
 micromamba config set channel_priority strict
 
 # Install omero-py and other packages
